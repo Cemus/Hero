b072926320b199d651edf7f30f8bfd95ed7c7794
kevin <24947582+Cemus@users.noreply.github.com>
2025-04-15 15:35:27 +0200

    Add stats condition in factory

--- Hero.API/Factories/ConditionFactory.cs
+++ Hero.API/Factories/ConditionFactory.cs
@@ -10,5 +10,35 @@ namespace Hero.API.Factories
                 ConditionType = new ConditionType { Name = "TestStrengthAbove" },
                 Value = value
             };
+        public static Condition DefenseAbove(int value) =>
+            new()
+            {
+                ConditionType = new ConditionType { Name = "TestDefenseAbove" },
+                Value = value
+            };
+        public static Condition DexterityAbove(int value) =>
+            new()
+            {
+                ConditionType = new ConditionType { Name = "TestDexterityAbove" },
+                Value = value
+            };
+        public static Condition KnowledgeAbove(int value) =>
+            new()
+            {
+                ConditionType = new ConditionType { Name = "TestKnowledgeAbove" },
+                Value = value
+            };
+        public static Condition VitalityAbove(int value) =>
+            new()
+            {
+                ConditionType = new ConditionType { Name = "TestVitalityAbove" },
+                Value = value
+            };
+        public static Condition CharismaAbove(int value) =>
+            new()
+            {
+                ConditionType = new ConditionType { Name = "TestCharismaAbove" },
+                Value = value
+            };
     }
 }
