c173fe2b507989041aff665c9fe717837b238ea8
kevin <24947582+Cemus@users.noreply.github.com>
2025-04-18 14:38:00 +0200

    Add FormModels folder

---
+++ Hero.Blazor/FormModels/CharacterFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hero.Blazor.FormModels
+{
+    public class CharacterFormModel
+    {
+        [Required(ErrorMessage = "Veuillez entrer un nom.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Veuillez choisir une classe.")]
+        public string? Job { get; set; }
+    }
+}
---
+++ Hero.Blazor/FormModels/SceneFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hero.Blazor.FormModels
+{
+    public class SceneFormModel
+    {
+        [Required(ErrorMessage = "La scène doit avoir un nom")]
+        [MinLengthAttribute(3, ErrorMessage = "Le nom de la scène doit avoir au moins 3 caractères")]
+        [MaxLengthAttribute(12, ErrorMessage = "Le nom de la scène doit avoir au moins 3 caractères")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "La scène doit avoir une description")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
