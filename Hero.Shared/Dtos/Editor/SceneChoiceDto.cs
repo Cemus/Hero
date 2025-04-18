a5877392f1df32f8eb533b42f39231558562fc9d
kevin <24947582+Cemus@users.noreply.github.com>
2025-04-18 17:02:34 +0200

    Split NewScene in components (Choice)

---
+++ Hero.Shared/Dtos/Editor/SceneChoiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Hero.Shared.Dtos.Editor
+{
+    public class SceneChoiceDto
+    {
+        public string Text { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public int? TargetSceneId { get; set; }
+        public int? TargetItemId { get; set; }
+    }
+}
